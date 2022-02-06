9c1f6bd703ce21211fba5a9a7fe3c2f2298a43cb
Giang Nguyen <giangvubinhn@vt.edu>
2022-02-06 10:10:49 -0500

    second finalizing

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import AddPill from './components/crud/AddPill';
+import {LogBox} from 'react-native'
 import Pill from './components/Pill';
 
-
+LogBox.ignoreAllLogs()
 const Stack = createStackNavigator();
 export default function App() {
-
 	return (
 		<NavigationContainer>
 			<Stack.Navigator screenOptions={{
